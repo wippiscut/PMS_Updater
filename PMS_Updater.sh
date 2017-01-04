732b050ee56dc1fe44b71c1ee4bbe14f8329c714
wippiscut <maverek5@gmail.com>
2017-01-04 17:37:11 -0600

    Update PMS_Updater.sh
    v2

--- PMS_Updater.sh
+++ PMS_Updater.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 URLBASIC="https://plex.tv/api/downloads/1.json"
-URLPLEXPASS="https://downloads.plex.tv/plex-media-server/1.2.0.3167-aa897d8/PlexMediaServer-1.2.0.3167-aa897d8-freebsd-amd64.tar.bz2"
+URLPLEXPASS="https://forums.plex.tv/discussion/250789/dvr-beta-four-redux-a-good-day-to-dvr-hard-special-bonus-directors-cut-extended-edition#latest"
 DOWNLOADPATH="/tmp"
 LOGPATH="/tmp"
 LOGFILE="PMS_Updater.log"
